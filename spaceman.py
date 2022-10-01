1d731b091b8fa96792dca62e97ae9bff0c466089
Andrew Pham <andrew.pham@shopify.com>
2022-09-30 21:06:33 -0400

    Add initial print statements and prompt function with validation for user guess

--- spaceman.py
+++ spaceman.py
@@ -2,49 +2,49 @@ import random
 
 
 def load_word():
-    '''
+    """
     A function that reads a text file of words and randomly selects one to use as the secret word
         from the list.
 
-    Returns: 
+    Returns:
            string: The secret word to be used in the spaceman guessing game
-    '''
-    f = open('words.txt', 'r')
+    """
+    f = open("words.txt", "r")
     words_list = f.readlines()
     f.close()
 
     # comment this line out if you use a words.txt file with each word on a new line
-    words_list = words_list[0].split(' ')
+    words_list = words_list[0].split(" ")
     secret_word = random.choice(words_list)
     return secret_word
 
 
 def is_word_guessed(secret_word, letters_guessed):
-    '''
+    """
     A function that checks if all the letters of the secret word have been guessed.
 
     Args:
         secret_word (string): the random word the user is trying to guess.
         letters_guessed (list of strings): list of letters that have been guessed so far.
 
-    Returns: 
+    Returns:
         bool: True only if all the letters of secret_word are in letters_guessed, False otherwise
-    '''
+    """
     # TODO: Loop through the letters in the secret_word and check if a letter is not in lettersGuessed
     pass
 
 
 def get_guessed_word(secret_word, letters_guessed):
-    '''
+    """
     A function that is used to get a string showing the letters guessed so far in the secret word and underscores for letters that have not been guessed yet.
 
-    Args: 
+    Args:
         secret_word (string): the random word the user is trying to guess.
         letters_guessed (list of strings): list of letters that have been guessed so far.
 
-    Returns: 
+    Returns:
         string: letters and underscores.  For letters in the word that the user has guessed correctly, the string should contain the letter at the correct position.  For letters in the word that the user has not yet guessed, shown an _ (underscore) instead.
-    '''
+    """
 
     # TODO: Loop through the letters in secret word and build a string that shows the letters that have been guessed correctly so far that are saved in letters_guessed and underscores for the letters that have not been guessed yet
 
@@ -52,7 +52,7 @@ def get_guessed_word(secret_word, letters_guessed):
 
 
 def is_guess_in_word(guess, secret_word):
-    '''
+    """
     A function to check if the guessed letter is in the secret word
 
     Args:
@@ -62,25 +62,44 @@ def is_guess_in_word(guess, secret_word):
     Returns:
         bool: True if the guess is in the secret_word, False otherwise
 
-    '''
+    """
     # TODO: check if the letter guess is in the secret word
 
     pass
 
 
+def prompt_letter():
+    while True:
+        user_letter = input("Enter a letter: ")
+        if len(user_letter) > 1:
+            print("Please only enter one letter at a time.")
+            continue
+        else:
+            break
+
+
 def spaceman(secret_word):
-    '''
+    """
     A function that controls the game of spaceman. Will start spaceman in the command line.
 
     Args:
       secret_word (string): the secret word to guess.
 
-    '''
+    """
+    num_of_guesses = 7
 
     # TODO: show the player information about the game according to the project spec
+    print("Welcome to Spaceman!")
+    print(f"The secret word contains: {len(secret_word)} letters.")
 
-    # TODO: Ask the player to guess one letter per round and check that it is only one letter
-
+    print(
+        f"You have {7} incorrect guesses, please enter one letter per round."
+    )
+    while num_of_guesses > 0:
+        print("-" * 37)
+        # TODO: Ask the player to guess one letter per round and check that it is only one letter
+        user_guess = prompt_letter()
+        num_of_guesses -= 1
     # TODO: Check if the guessed letter is in the secret or not and give the player feedback
 
     # TODO: show the guessed word so far
